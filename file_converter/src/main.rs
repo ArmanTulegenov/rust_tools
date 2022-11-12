3dcf65a92e973f4948076a6b0ea2025b87a8cb3a
Arman Tulegenov <arman.tulegenov@gmail.com>
2022-11-12 23:33:22 +0300

    read current folder

--- file_converter/src/main.rs
+++ file_converter/src/main.rs
@@ -4,11 +4,17 @@ use tokio::io::{self, AsyncReadExt};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut entries = fs::read_dir(".").await?;
 
+    let dir_path = std::env::args().nth(1).expect("Please provide a directory as command line argument.");
+    let pattern = std::env::args().nth(2).expect("Please provide a pattern as command line argument.");
+
+    let mut entries = fs::read_dir(&dir_path).await?;
     while let Some(entry) = entries.next_entry().await? {
-        println!("{:?}", entry.path());
+        if entry.file_name().to_string_lossy().contains(&pattern) {
+            println!("{:?}", entry.path());
+        }
     }
     Ok(())
 
+    
 }
