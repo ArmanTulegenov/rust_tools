c453a6ef0c0ab27bf8439bc71df90bea1b2ca87c
Arman Tulegenov <arman.tulegenov@gmail.com>
2022-11-13 23:40:18 +0300

    add gz filter and queue

--- file_converter/src/main.rs
+++ file_converter/src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate queues;
+
+use std::path::PathBuf;
+
+use queues::{Queue, IsQueue};
 use tokio;
 use tokio::fs;
 use tokio::io::{self, AsyncReadExt};
@@ -5,16 +11,26 @@ use tokio::io::{self, AsyncReadExt};
 #[tokio::main]
 async fn main() -> io::Result<()> {
 
+    let mut q: Queue<PathBuf> = queue![];
+
     let dir_path = std::env::args().nth(1).expect("Please provide a directory as command line argument.");
     let pattern = std::env::args().nth(2).expect("Please provide a pattern as command line argument.");
 
     let mut entries = fs::read_dir(&dir_path).await?;
     while let Some(entry) = entries.next_entry().await? {
-        if entry.file_name().to_string_lossy().contains(&pattern) {
-            println!("{:?}", entry.path());
+        let file_name = entry.file_name().to_string_lossy().to_string();
+        if file_name.contains(&pattern) {
+            if let Some(e) = entry.path().extension() { 
+                if e.to_string_lossy().to_string() == "gz" {
+                    q.add(entry.path());
+                    println!("{:?}", entry.path());
+                } else {
+                    println!("{:?}", entry.path());
+                }
+            }
         }
     }
-    Ok(())
 
+    Ok(())
     
 }
