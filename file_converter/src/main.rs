3f6970d844dccf1818693f29e016970b6046a30c
Arman Tulegenov <arman.tulegenov@gmail.com>
2022-11-15 18:54:30 +0300

    add args and json path support

--- file_converter/src/main.rs
+++ file_converter/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{Read, BufReader};
 use std::path::PathBuf;
 
+use async_recursion::async_recursion;
 use flate2::read::GzDecoder;
 use tokio;
 use tokio::fs;
@@ -9,20 +10,39 @@ use tokio::io;
 use itertools::Itertools;
 use futures::stream::FuturesUnordered;
 use serde_json::{Result, Value};
-
+use clap::{arg, command, value_parser, ArgAction, Command};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
 
+    let matches = command!() // requires `cargo` feature
+        .arg(
+            arg!(
+                -d --directory <DIRECTORY> "A path to directory with json files."
+            )
+            // We don't have syntax yet for optional options, so manually calling `required`
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .arg(arg!(
+            -p --prefix <PREFIX> "A file name prefix to filter."
+        ))
+        .arg(arg!(
+            -f --filter <FILTER> "Json fields to filter, e.g gtp-handler,gx-client."
+        ).required(false)
+        )
+        .get_matches();
+    
+
+    let dir_path: &PathBuf = matches.get_one::<PathBuf>("directory").unwrap();
+    let prefix: &str = matches.get_one::<String>("prefix").unwrap();
+    let filter = matches.get_one::<String>("filter");
+
     let mut q: Vec<PathBuf> = vec![];
 
-    let dir_path = std::env::args().nth(1).expect("Please provide a directory as command line argument.");
-    let pattern = std::env::args().nth(2).expect("Please provide a pattern as command line argument.");
-
-    let mut entries = fs::read_dir(&dir_path).await?;
+    let mut entries = fs::read_dir(&dir_path.as_path()).await?;
     while let Some(entry) = entries.next_entry().await? {
         let file_name = entry.file_name().to_string_lossy().to_string();
-        if file_name.contains(&pattern) {
+        if file_name.contains(&prefix) {
             q.push(entry.path());
         }
     }
@@ -78,8 +98,48 @@ async fn process_text_lines(lines: &str ) -> io::Result<()> {
     
     for line in lines.lines() {
         let v: Value = serde_json::from_str(line)?;
-        println!("{:?}", v);
+        let mut prefix = "";
+        process_json_value(&v, &prefix).await;
     }
 
     Ok(())
+}
+
+#[async_recursion]
+async fn process_json_value(v: &Value, prefix: &str) -> io::Result<()> {
+    
+    match v {
+        Value::Null  => {
+            println!("Null value")
+        },
+        Value::Bool(value) => {
+            println!("value {:?}", value)
+        },
+        Value::Number(value) => {
+            println!("value {:?}", value)
+        },
+        Value::String(value) => {
+            println!("value {:?}", value)
+        },
+        Value::Array(values) => { 
+            for value in values.iter() {
+                process_json_value(&value, &prefix);
+            }
+        },
+        Value::Object(map) => { 
+            for (key, value) in map.iter() {
+                let new_prefix;
+                if "" != prefix {
+                    new_prefix = String::from(prefix) + "."  + key;
+                } else {
+                    new_prefix = String::from(key);
+
+                }
+                print!("entry {:?} ", new_prefix);
+                process_json_value(&value, &new_prefix).await;
+            }
+        },
+    }
+    Ok(())
+
 }
